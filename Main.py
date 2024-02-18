ad41a4a2d369a6ffa482cfdc480912612ae52ad4
gabeweng <gabeweng@gmail.com>
2024-02-17 23:08:45 -0500

    import all libraries

---
+++ Main.py
@@ -0,0 +1,14 @@
+import os
+import textwrap
+
+import chromadb
+import langchain
+import openai
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader, UnstructuredPDFLoader, YoutubeLoader
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
